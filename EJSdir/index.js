9de1e53b25e3dc4c044df8ab716f389f395c51de
Aman Dhaka <asdhaka9050@gmail.com>
2024-10-29 18:06:03 +0530

    learning backend

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -18,11 +18,11 @@ app.get("/ig/:username",(req,res)=>{
     const followers=["adam","bob","steve","abc"]
     let {username}= req.params;
     res.render("instagram.ejs",{username,followers});
-})
+});
 app.get("/rolldice",(req,res)=>{
     let diceVal = Math.floor(Math.random()*6)+1;
     res.render("rolldice.ejs",{diceVal});
-})
+});
 app.get("/hello",(req,res)=>{
     res.send("hello");
 });
